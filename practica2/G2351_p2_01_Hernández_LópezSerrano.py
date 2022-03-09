df7ef48bebafa3363d493c213d880b7471d3f931
Manu <manuloseta@gmail.com>
2022-03-09 14:03:46 +0100

    empezando la heuristica

--- practica2/G2351_p2_01_Hernández_LópezSerrano.py
+++ practica2/G2351_p2_01_Hernández_LópezSerrano.py
@@ -3,6 +3,7 @@ import numpy as np
 from game import (
     TwoPlayerGameState,
 )
+from reversi import from_dictionary_to_array_board
 from tournament import (
     StudentHeuristic,
 )
@@ -29,9 +30,17 @@ class Solution3(StudentHeuristic):
   def get_name(self) -> str:
     return "solution3"
   def evaluation_function(self, state: TwoPlayerGameState) -> float:
-    # let's use an auxiliary function
-    aux = self.dummy(321)
-    return aux
+    board_array = from_dictionary_to_array_board(state.board, 8 ,8)
+    print(board_array)
+    print(state.board)
 
-  def dummy(self, n: int) -> int:
-    return n + 15
+    return 1
+
+def is_border(position=(1,1), height=8, width=8) -> bool:
+  x,y=position[0],position[1]
+  return (x==1 or y==1 or x==width or y==height)
+
+def is_corner(position=(1,1), height=8, width=8) -> bool:
+  x,y=position[0],position[1]
+  corners = [(1,1),(1,8),(8,1),(8,8)]
+  return (position in corners)
